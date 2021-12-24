32dc0919d31750cf8d2f12c856721fb5af2bf96e
GribanovKirill <92813430+GribanovKirill@users.noreply.github.com>
2021-12-24 20:45:21 +0500

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, b = 1, c, sum = 0;
+            Console.WriteLine("Введите n:");
+            n = Convert.ToDouble(Console.ReadLine());
+            while (b <= n)
+            {
+                c = 1 / b;
+                sum += c;
+                b++;
+            }
+            Console.WriteLine($"Числовой ряд равен {sum}");
+            Console.ReadKey();
+        } 
+    }
+}
